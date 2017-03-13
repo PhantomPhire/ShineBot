697918e46a22b2c2f726daa50bf6d7fc1e56c6f0
Michael Oltmanns <mikeolt7@gmail.com>
2017-03-13 12:31:25 -0700

    Added a note to getGuild.js

--- shineBot_util/getGuild.js
+++ shineBot_util/getGuild.js
@@ -1,3 +1,16 @@
+/*
+The reason for the existence of this file is to ensure that the bot always has
+ access to the guild this bot is intended for, even when operating outside of 
+ the guild (such as a DM). 
+
+ Right now, this is implemented in a not so good way. It relies on the
+ assumption that said guild is the only guild the bot is in or that the guild
+ in question is first in the array. A better solution would be to serialize 
+ the guild as a .json and read from that upon start-up. Will do later. For
+ now, just chalk it up to my laziness.
+
+ -SweetWilly
+*/
 var client = null;
 
 module.exports.giveClient = function(clientIn) {
